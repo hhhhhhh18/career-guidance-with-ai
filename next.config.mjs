12d2c8aca5fc1544b14729fd6ffeb8cb94e9d9f8
hhhhhhh18 <shaikhamad42792@gmail.com>
2025-10-11 12:00:49 +0530

    Expose DATABASE_URL in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-      },
-    ],
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 };
 
